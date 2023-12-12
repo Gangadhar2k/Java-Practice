7282117c5fa42042fe3f7b4192ee289c50304227
gangadhar2k <gangadhar2k.official@gmail.com>
2023-12-12 13:51:10 +0530

    Imperative vs Declarative Approach

---
+++ com/functionalProgramming/Imperative_VS_Declarative.java
@@ -0,0 +1,66 @@
+package com.functionalProgramming;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Imperative_VS_Declarative {
+
+    public static void main(String[] args) {
+        List <Person> people = List.of(
+                new Person("Gangadhar",Gender.MALE),
+                new Person("Aisha",Gender.FEMALE),
+                new Person("Mari",Gender.MALE),
+                new Person("Lara",Gender.FEMALE),
+                new Person("Muthu",Gender.MALE),
+                new Person("Reka",Gender.FEMALE)
+
+        );
+
+
+        //imerative Approach
+        System.out.println("------------- //imerative Approach------------");
+        List<Person> females = new ArrayList<>();
+
+        for (Person person : people) {
+            if( Gender.FEMALE.equals(person.gender) ){
+                females.add(person);
+            }
+        }
+
+        for (Person female: females) {
+            System.out.println(female);
+        }
+
+        // Declarative Approach
+        System.out.println("---------// Declarative Approach----------");
+        List<Person> females2 = people.stream()
+                .filter(person -> Gender.FEMALE.equals(person.gender) )
+                .toList();
+
+        females2.forEach(System.out::println);
+
+
+    }
+
+    static class Person{
+        private final String name;
+        private final  Gender gender;
+
+        public Person(String name, Gender gender) {
+            this.name = name;
+            this.gender = gender;
+        }
+
+        @Override
+        public String toString() {
+            return "Person{" +
+                    "name='" + name + '\'' +
+                    ", gender=" + gender +
+                    '}';
+        }
+    }
+    enum Gender{
+        MALE,FEMALE
+    }
+}
